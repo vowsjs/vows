7ce45791dc59ea8b0dac7be8800d6e71bf3b2818
cloudhead <self@cloudhead.net>
2010-06-04 01:27:46 -0400

    another test, just to test runner

---
+++ test/other-test.js
@@ -0,0 +1,15 @@
+var path = require('path');
+
+require.paths.unshift(path.join(__dirname, '..', 'lib'));
+
+var assert = require('assert');
+var vows = require('vows');
+
+vows.describe("Vows/other").addVows({
+    "Another test": {
+        topic: true,
+        "exists in the test/ folder!": function (topic) {
+            assert.ok (topic);
+        }
+    }
+}).export(this);
