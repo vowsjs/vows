ebf2bf6a2b5969249a373ba9554df604e35bdb9a
Locksfree <locksfree@gmail.com>
2011-08-30 00:27:51 +0100

    Handle the case where a topic function throws.
    If a topic function throws, instead of stopping the whole testing
    process, it now reports all associated vows as failing.

--- test/vows-error-test.js
+++ test/vows-error-test.js
@@ -22,7 +22,7 @@ function doSomethingAsyncWithError(callback) {
   	});
 }
 
-
+var batchCount = 0;
 vows.describe('vows/error').addBatch({
 	'Generate success response to async function': {
     		topic: function() {
@@ -47,5 +47,43 @@ vows.describe('vows/error').addBatch({
     			  // This assertion fails. It shouldn't.
     			  assert.equal(testValue, 'a');
     		}
-  	}
-}).export(module)
+  	},
+
+   'Generate a hard error in the topic': {
+       topic: function(){ ({}).unknownMethod(this.callback); },
+       'should not stop vows': function(){ assert.equal(arguments.length,1); }
+   },
+
+   'In a batch': {
+       'generate an exception in the first topic': {
+           topic: function(){ ({}).unknownMethod(this.callback); },
+           'vow': function(){ ++batchCount; }
+       },
+       'ensure the second topic runs': {
+           topic: function(){ return {}; },
+           'vow': function(){ ++batchCount; },
+       }
+   }
+}).addBatch({
+   'Next batch running sequentially': {
+       topic: function(){ return batchCount; },
+       'checks that the batchCount is 1': function(b){ assert.equal(b, 1); }
+   }
+}).addBatch({
+    'Outer batch': {
+        'Nested batch': {
+            'Nested(2) batch: topic throws': {
+                topic: function(){ ({}).unknownMethod(this.callback); },
+                'vow 1': function(){},
+                'vow 2': function(){},
+            }
+        },
+        'Nested(2) batch decreases batchCount': {
+            topic: function(){ batchCount--; return batchCount; },
+            'vow': function(b){ assert.equal(b, 0); },
+        }
+    }
+}).export(module);
+
+
+
