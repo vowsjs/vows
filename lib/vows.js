vowsjs/vows

commit 692cb71ec2100e0f1dcb3add2d14dc93ef864e9f

try to handle async errors more intelligently

lib/vows.js
@@ -134,10 +134,9 @@ process.addListener('exit', function () {
     vows.suites.forEach(function (s) {
         if ((s.results.total > 0) && (s.results.time === null)) {
             vows.reporter.report(['error', { error: "Asynchronous Error", suite: s }]);
-            process.exit(1);
         }
         s.batches.forEach(function (b) {
-            if (b.status !== 'end') {
+            if (b.status === 'begin') {
                 failure = true;
                 results.errored ++;
                 results.total ++;

lib/vows/suite.js
@@ -104,6 +104,8 @@ this.Suite.prototype = new(function () {
             tests   = batch.tests,
             promise = batch.promise = new(events.EventEmitter);
 
+        batch.status = 'begin';
+
         // The test runner, it calls itself recursively, passing the
         // previous context to the inner contexts. This is so the `topic`
         // functions have access to all the previous context topics in their

commit 38817c1b2670db45e982b52b30ae044e6c683130

Exit code should be 1 if asynchronous errors occurs
Regression introduced in 692cb71ec2100e0f1dcb3add2d14dc93ef864e9f.

Fixes #4.

lib/vows.js
@@ -202,6 +202,7 @@ process.on('exit', function () {
     });
     if (failure) {
         util.puts(console.result(results));
+        process.exit(1);
     }
 });
 