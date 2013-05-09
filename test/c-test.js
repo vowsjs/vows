3f585a0fe1850874b7fd75f5821752546bb92769
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-05-08 20:50:45 -0400

    used JSHint to apply consistent indentation

--- test/c-test.js
+++ test/c-test.js
@@ -1,14 +1,13 @@
 var vows = require('../lib/vows'),
-    assert = require('assert');
+assert = require('assert');
 
 vows.describe("Vows test file starts with c").addBatch({
-
-  "The test file": {
-    topic: function () {
-        return { flag: true };
-    },
-    "is run": function (topic) {
-        assert.isTrue(topic.flag);
+    "The test file": {
+        topic: function () {
+            return { flag: true };
+        },
+        "is run": function (topic) {
+            assert.isTrue(topic.flag);
+        }
     }
-  }
 }).export(module);
--- test/fixtures/isolate/log.js
+++ test/fixtures/isolate/log.js
@@ -1,18 +1,18 @@
 var vows = require('../../../lib/vows'),
-    assert = require('assert');
+assert = require('assert');
 
 var obvious;
 vows.describe('stderr').addBatch({
-  'Obvious test': obvious = {
-    topic: function () {
-      this.callback(null, true);
+    'Obvious test': obvious = {
+        topic: function () {
+            this.callback(null, true);
+        },
+        'should work': function (result) {
+            console.log('oh no!');
+            assert.ok(result);
+        }
     },
-    'should work': function (result) {
-      console.log('oh no!');
-      assert.ok(result);
-    }
-  },
-  'Obvious test #2': obvious,
-  'Obvious test #3': obvious,
-  'Obvious test #4': obvious
+    'Obvious test #2': obvious,
+    'Obvious test #3': obvious,
+    'Obvious test #4': obvious
 }).export(module);
