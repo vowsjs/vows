ca4eee080c3eaaf6a94e6a441c1efd6e184e0b7c
Jerry Sievert <code@legitimatesounding.com>
2013-11-18 21:31:01 -0800

    more tests

--- test/suite-test.js
+++ test/suite-test.js
@@ -9,6 +9,25 @@ vows.describe("Suite").addBatch({
     },
     "should be an instance of Suite": function (topic) {
       assert.isTrue(topic instanceof suite.Suite);
+    },
+    "should have an internal name of 'test'": function (topic) {
+      assert.equal(topic.name, "test");
+    },
+    "with added batches": {
+      topic: function (topic) {
+        topic.addBatch({
+          "foo": {
+            topic: function ( ) {
+              return "bar";
+            }
+          }
+        });
+
+        return topic;
+      },
+      "should contain a batch": function (topic) {
+        assert.equal(topic.batches.length, 1);
+      }
     }
   }
 }).export(module);
