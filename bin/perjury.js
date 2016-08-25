b5ab61b5fc8dfbf6528adef297b6b7fb1fa06c88
Evan Prodromou <evan@fuzzy.ai>
2016-08-25 11:31:27 -0400

    Mark perjury.js as es6 for jshint

--- bin/perjury.js
+++ bin/perjury.js
@@ -14,6 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/* jshint esversion: 6 */
+
 const path = require('path');
 
 const argv = require('yargs')
