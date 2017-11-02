6d66010bb4b117cd17162e7afb06add842a3cfe6
Evan Prodromou <evan@fuzzy.ai>
2017-11-01 20:16:58 -0400

    Install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "standard",
+    "rules": {
+      "no-var": ["error"],
+      "prefer-const": ["error"],
+      "prefer-arrow-callback": ["error"],
+      "prefer-rest-params": ["error"],
+      "prefer-spread": ["error"],
+      "prefer-template": ["error"],
+      "prefer-destructuring": ["error"]
+    }
+};
