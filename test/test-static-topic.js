644b5b62c85f227f49dae8950bb6fc78497ff0cf
Evan Prodromou <evan@prodromou.name>
2018-05-07 16:05:18 -0400

    Handle non-function topics; closes #396

---
+++ test/test-static-topic.js
@@ -0,0 +1,33 @@
+// test-static-topic.js -- A topic that is not a function
+//
+// Copyright 2018 Fuzzy.ai <evan@fuzzy.ai>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict'
+
+const vows = require('../lib/index')
+const assert = vows.assert
+
+vows.describe('topic that is not a function')
+  .addBatch({
+    'When we use a static topic': {
+      topic: 42,
+      'it works' (err, data) {
+        assert.ifError(err)
+        assert.isNumber(data)
+        assert.equal(data, 42)
+      }
+    }
+  })
+  .export(module)
